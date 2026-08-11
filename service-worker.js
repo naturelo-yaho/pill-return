3ed510f39d1bca9854187f38d7a9c6bf6d1d2362
naturelo-yaho <naturelo@hanmail.net>
2026-08-11 15:28:50 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='pill-return-v16';
+const CACHE='pill-return-v17-photo-history';
 const STATIC=['./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
