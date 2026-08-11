ed7c24b5d6e4903401142a7e4571eeb91b6cb0f1
naturelo-yaho <naturelo@hanmail.net>
2026-08-11 16:58:04 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='pill-return-v23-displayfix';
+const CACHE='pill-return-v25-wholesaler';
 const SHARE_CACHE='pill-return-shares';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
